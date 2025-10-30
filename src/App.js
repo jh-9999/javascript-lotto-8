14bfe4f2134e574a2c6253e7b3df5b330e468731
이정현 <wjd1gus2@gmail.com>
2025-10-30 17:06:59 +0900

    feat(app): add function to receive user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils"
+
+const PLACEHOLDER_BUY_LOTTO = "구입금액을 입력해 주세요.\n";
+
 class App {
-  async run() {}
+  async run() {
+    const buyLotto = await userInput(PLACEHOLDER_BUY_LOTTO);
+  }
+}
+
+async function userInput(placeholder = "") {
+  return await Console.readLineAsync(placeholder);
 }
 
 export default App;
