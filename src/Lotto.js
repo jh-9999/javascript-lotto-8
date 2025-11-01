d8d1b3381f4e16835c926b98e1cdc9568bbc8762
이정현 <wjd1gus2@gmail.com>
2025-11-01 17:22:37 +0900

    feat(lotto): add function to check numbers between 1 and 45

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,6 +1,8 @@
 const ERROR_MESSAGE = {
   INVALID_LENGTH_OF_NUMBERS: "[ERROR] 로또 번호는 6개여야 합니다.",
-  INVALID_DUPLICATE_NUMBERS: "[ERROR] 로또 번호에 중복된 숫자가 있습니다."
+  INVALID_DUPLICATE_NUMBERS: "[ERROR] 로또 번호에 중복된 숫자가 있습니다.",
+  INVALID_NUMBER_RANGE: "[ERROR] 로또 번호는 1~45 사이의 숫자여야 합니다."
+
 }
 const LOTTO_NUMBER_COUNT = 6;
 
@@ -15,6 +17,7 @@ class Lotto {
   #validate(numbers) {
     this.validateLengthOfNumbers(numbers);
     this.validateDuplicateNumbers(numbers);
+    this.validateNumberRange(numbers);
   }
 
   // TODO: 추가 기능 구현
@@ -33,7 +36,15 @@ class Lotto {
   validateDuplicateNumbers(numbers) {
     for (let i = 0; i < numbers.length; i++) {
       for (let j = i + 1; j < numbers.length; j++) {
-        this.checkSameNumber(numbers[i], numbers[j]);
+        this.validateSameNumber(numbers[i], numbers[j]);
+      }
+    }
+  }
+  validateNumberRange(numbers){
+    for (let number of numbers){
+      number = Number(number);
+      if (number < 1 || number > 45) {
+        throw new Error(ERROR_MESSAGE.INVALID_NUMBER_RANGE);
       }
     }
   }
